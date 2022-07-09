f1245c22d017dd0c67829e1223904b582773c1e5
Andreixzc <andrei.massaini@hotmail.com>
2022-07-09 16:17:43 -0300

    fix

--- ExpressoFiesta/src/main/java/service/AtracaoService.java
+++ ExpressoFiesta/src/main/java/service/AtracaoService.java
@@ -17,10 +17,18 @@ public class AtracaoService {
 	
 	
 	private AtracaoDAO atracaoDAO = new AtracaoDAO();
-	
+
 	private void setReponseHeaders(Response response) {
 		response.header("Content-Type", "application/json");
 		response.header("Content-Encoding", "UTF-8");
+		response.header("Access-Control-Allow-Credentials", "true");
+        response.header("Access-Control-Allow-Origin", "*");
+        response.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
+        response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, "
+                + "Content-Type, Accept, X-CSRF-TOKEN, Cache-Control, DNT, X-CustomHeader, Keep-Alive, "
+                + "User-Agent, If-Modified-Since, Content-Range, Range");
+        response.header("Access-Control-Max-Age", "3600");
+        response.type("application/json");
 	}
 	
 	private Atracao buscarPorId(Request request) {
