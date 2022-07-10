cd40939859ba20be7d2c3097af4a70631613bb53
Andreixzc <andrei.massaini@hotmail.com>
2022-07-09 21:17:23 -0300

    d

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -16,11 +16,22 @@ public class Aplicacao {
 	private static LocalService localService = new LocalService();
 	private static PedidoService pedidoService = new PedidoService();
 	private static UsuarioService usuarioService = new UsuarioService();
+	 static int getHerokuAssignedPort() {
+	        ProcessBuilder processBuilder = new ProcessBuilder();
+	        if (processBuilder.environment().get("PORT") != null) {
+	            return Integer.parseInt(processBuilder.environment().get("PORT"));
+	        }
+	        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	    }
     public static void main(String[] args) {
-       port(8080);
+    	port(getHerokuAssignedPort());
+    		
+    	
+//       port();
       
 ///////////////////////////////////////////////////////AlimentoService////////////////////////////////////////
-	   get("/alimento/list", (request, response) -> alimentoService.getAll(request, response));
+ 
+       get("/alimento/list", (request, response) -> alimentoService.getAll(request, response));
 
 	   get("/alimento/:id", (request, response) -> alimentoService.get(request, response));
 	   
